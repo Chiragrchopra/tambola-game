70a32d69993ffb338ede2f6e5da7509a645a480d
Chirag Chopra <iamchiragchopra@gmail.com>
2021-09-01 13:08:14 +0530

    added domain to game link

--- landing/js/main.js
+++ landing/js/main.js
@@ -17,6 +17,6 @@ window.onload = function () {
   var newRoomBtn = document.getElementById("generate-new-room");
   newRoomBtn.onclick = () => {
     let uniqueRoomId = "/game/" + uniqueid();
-    location.href = location.href + uniqueRoomId;
+    location.href = 'http://www.eutsav.in' + uniqueRoomId;
   };
 };
